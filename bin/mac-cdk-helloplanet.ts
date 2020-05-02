b1d4b026e860c3e0b0561c9415412428a3e00a13
Mac <mac.kittipat@gmail.com>
2020-05-02 21:45:07 +0700

    Initial commit

---
+++ bin/mac-cdk-helloplanet.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { MacCdkHelloplanetStack } from '../lib/mac-cdk-helloplanet-stack';
+
+const app = new cdk.App();
+new MacCdkHelloplanetStack(app, 'MacCdkHelloplanetStack');
